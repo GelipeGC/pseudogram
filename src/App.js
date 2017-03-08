9017f15944083b3326595785a0d90d91c081b4d3
Felipe Guzman <felipe-guzman.c@hotmail.com>
2017-03-08 01:19:19 -0600

    add integracion de firebase y autenticacion con google

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      user: null
+    };
+  }
+
+  componentWillMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout () {
+
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha cerrado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton () {
+    //si el usuario está logeado
+    if (!this.state.user) {
+      return (
+          <button onClick={this.handleAuth}>
+            Login con Google 
+          </button>
+        );
+      } else {
+       //si no lo está
+       return (
+        <div>
+          <p>Hola {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}>Salir</button>
+        </div>
+        );
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +62,7 @@ class App extends Component {
           <h2>Welcome to Pseudogram</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         { this.renderLoginButton()}
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import App from './App';
 import './index.css';
+
+firebase.initializeApp({
+	apiKey: "AIzaSyAIvw5gRmS1glX4H7uxYy3-32zDwW3ytVQ",
+    authDomain: "pseudogram-538d1.firebaseapp.com",
+    databaseURL: "https://pseudogram-538d1.firebaseio.com",
+    storageBucket: "pseudogram-538d1.appspot.com",
+    messagingSenderId: "471927577678"
+});
 
 ReactDOM.render(
   <App />,
