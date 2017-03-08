100ef2d91ae92b5e159752573eccd3ce48b892d7
Felipe Guzman <felipe-guzman.c@hotmail.com>
2017-03-08 02:10:47 -0600

    Almacenamiento de datos con firebase database en tiempo real

--- src/App.js
+++ src/App.js
@@ -9,8 +9,13 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-      user: null
+      user: null,
+      pictures: []
     };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
   }
 
   componentWillMount () {
@@ -18,8 +23,11 @@ class App extends Component {
       this.setState({ user });
     });
 
-    this.handleAuth = this.handleAuth.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
+    firebase.database().ref('pictures').on('child_added', snapshot => {
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      });
+    }); 
   }
 
   handleAuth () {
@@ -37,21 +45,61 @@ class App extends Component {
       .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
 
+  handleUpload (event) {
+    const file = event.target.files[0];
+    const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
+    const task = storageRef.put(file);
+
+    task.on('state_changed', snapshot => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      this.setState({
+        uploadValue: percentage
+      })
+    }, error =>{
+      console.log(error.message)
+    }, () => {
+      const record = {
+        photoURL: this.state.user.photoURL,
+        displayName: this.state.user.displayName,
+        image: task.snapshot.downloadURL
+      };
+
+      const dbRef = firebase.database().ref('pictures');
+      const newPicture = dbRef.push();
+      newPicture.set(record);
+    });
+  }
+
   renderLoginButton () {
     //si el usuario está logeado
     if (!this.state.user) {
       return (
-          <button onClick={this.handleAuth}>
+          <button onClick={this.handleAuth} className="App-btn">
             Login con Google 
           </button>
         );
       } else {
        //si no lo está
        return (
-        <div>
-          <p>Hola {this.state.user.displayName}!</p>
-          <button onClick={this.handleLogout}>Salir</button>
-          <FileUpload />
+        <div className="App-intro">
+          <p className="App-intro">Hola {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout} className="App-btn">Salir</button>
+
+          <FileUpload onUpload={this.handleUpload}/>
+
+          {
+            this.state.pictures.map(picture => (
+              <div className="App-card">
+                <figure className="App-card-image">
+                  <img width="320" src={picture.image} alt={picture.displayName}/>
+                  <figCaption className="App-card-footer">
+                    <img className="App-card-avatar" src={picture.photoURL} alt={picture.displayName} />
+                    <span className="App-card-name">{picture.displayName}</span>
+                  </figCaption>
+                </figure>
+              </div>
+            )).reverse()
+          }
         </div>
         );
     }
